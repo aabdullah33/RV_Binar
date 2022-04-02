555693879d8e24e4d31d77f0ce276bad4ff969b6
aabdullah33 <a.abdullah3@gmail.com>
2022-04-02 23:01:06 +0700

    Initial commit

---
+++ app/src/main/java/com/binar/rv_binar/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.binar.rv_binar
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
